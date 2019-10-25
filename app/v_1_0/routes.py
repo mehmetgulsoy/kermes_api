5e2e2145aef18f9ccb766d1ab3e7042b394c5e37
Mehmet Gülsoy <m.mehmetgulsoy@gmail.com>
2019-10-25 15:33:57 +0300

    cors cookie desteği bad_request fonksiyonu

--- app/v_1_0/routes.py
+++ app/v_1_0/routes.py
@@ -1,42 +1,37 @@
-from flask import request, render_template ,jsonify
-from flask_login import LoginManager, login_user, current_user, logout_user
-from . import main
-from .model import Uye
-from .db import query_db
-
-@main.route('/')
-def index():
-  return render_template('index.html')
-
+from flask import request
+from flask_login import login_user, current_user, logout_user
+from app.v_1_0 import main
+from app.v_1_0.model import Uye
+from app.v_1_0.db import query_db
+from app.v_1_0.responce import bad_request, success_request
 
 @main.route('/login', methods=["POST","GET"])
 def login():
-  if current_user.is_authenticated:
-      return {'error': False, 'msg': 'Uye zaten login!'},200   
-
-  data = request.get_json() 
+  if current_user.is_authenticated:      
+      return success_request('Zaten login oldunuz.')   
   
-  if data['uye'] and data['sifre']: 
-    uye = data.get('uye','')
-    if '@' not in uye:
-      return {'error': True, 'msg': 'uye@firma formatında olmalı!'}, 400   
+  data = request.get_json(silent=True) or {}
+  if 'uye' not in data or 'sifre' not in data:     
+    return bad_request('uye ve/veya sifre gerekli!') 
 
-    uye, firma = str.split(uye,'@')
-    sifre = data.get('sifre')
+  if '@' not in data['uye']:
+    return bad_request('uye@firma formatında olmalı!') 
+ 
+  uye, firma = str.split(data['uye'],'@',1)
+  sifre = data['sifre']
 
-    if not uye or not firma or not sifre:
-      return {'error': True, 'msg': 'Kullacıcı adı ve/veya şifre giriniz!'},400    
+  if not uye or not firma or not sifre:
+    return bad_request('Kullanıcı adı ve/veya şifre hatalı!')    
     
-    uye = Uye.fromFirmaUye(firma,uye)
-    if uye is None or not uye.verify_password(sifre):
-      return {'error': True, 'msg': 'Kullacıcı adı ve/veya şifre hatalı'},400  
-    else:
-      ogin_user(uye, remember= False)
-      return jsonify({'error': False, 'msg': 'Login oldunuz', 'data': {'uye': uye.get_id()}}),200 
+  uye = Uye.fromFirmaUye(firma,uye)
+  if uye is None or not uye.verify_password(sifre):
+    return bad_request('Kullacıcı adı ve/veya şifre eşleşmedi.!')  
+  else:
+    login_user(uye)
+    data = { 'uye': uye.get_id() }
+    return success_request('Login oldunuz',data)
 
 @main.route('/logout')
 def logout():
-  error = False
-  msg  = 'Çıkış yapıldı'
   logout_user()
-  return {'error': error, 'msg': msg}
+  return success_request() 
